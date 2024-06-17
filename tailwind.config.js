559e18f3da329080f05f4c81711013a063509cf2
Angel silva <angelsilvadesantana@gmail.com>
2024-06-17 10:34:05 -0300

    Desktop version

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
